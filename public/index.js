488fbd0ef2e0d2fb497ea3db29c96ad0b234e9b7
Robert Hoffmann <robert.hoffmann@telekom.de>
2019-08-04 19:59:01 +0200

    Add simple i18n

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
 window.onload = function () {
+    domI18n({
+        languages: ['en', 'de']
+    });
     document.getElementById('file').onchange = function () {
         var filename = this.value.replace(/.*[\/\\]/, '')
         document.getElementById('filename').textContent = filename
